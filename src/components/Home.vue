5ce3aa3ba9b73e6b5c3749672e87a631650da1a5
ma06rii <ma06rii@live.co.uk>
2024-03-16 01:52:17 +0000

    Update text errror

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@ import { RouterLink } from 'vue-router'
             Sign your work. <br> Beat the spam.
         </h1>
         <p class="text-xl mb-8">
-            Proofly empowers creators and businesses to add a cryptographically verifiable signature to all the
+            Certted empowers creators and businesses to add a cryptographically verifiable signature to all the
             content they release online.
         </p><button
             class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 bg-white text-[#6f42c1] py-3 px-8 rounded-full"><RouterLink to="/signlink" >Try It Today</RouterLink></button>
